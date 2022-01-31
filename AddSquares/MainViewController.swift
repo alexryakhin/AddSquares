eedc898b8377e816bda7c27045fdb05a53ede5e3
Alexander Ryakhin <a.ryahin@trinitydigital.ru>
2022-01-31 13:22:01 +0300

    added marks

--- AddSquares/MainViewController.swift
+++ AddSquares/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  AddSquares
 //
 //  Created by Alexander Ryakhin on 1/31/22.
@@ -8,21 +8,25 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    // MARK: - Buttons outlet collection
+    // MARK: - IBOutlets
     @IBOutlet private var colorButtons: [UIButton]!
     
+    // MARK: - Private properties
     private var selectedColor: UIColor = .black
 
+    // MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    // MARK: IBActions
     @IBAction func colorButtonPressed(_ sender: UIButton) {
         guard let color = sender.backgroundColor else { return }
         selectedColor = color
         updateSelectedButton(sender)
     }
     
+    // MARK: - Private methods
     private func updateSelectedButton(_ button: UIButton) {
         colorButtons.forEach { $0.alpha = 1.0 }
         button.alpha = 0.8
